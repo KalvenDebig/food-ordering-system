dd04ffe6404dbf9ba46e95827dc81c36c965234c
Sijie Shen <shen.sij@northeastern.edu>
2023-04-05 14:04:31 -0400

    added Kafka config data generic module. closes #19.

---
+++ infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/kafka/config/data/KafkaConfigData.java
@@ -0,0 +1,20 @@
+package com.food.ordering.system.kafka.config.data;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author kalvens on 4/5/23
+ * @project food-ordering-system
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "kafka-config")
+public class KafkaConfigData {
+    private String bootstrapServers;
+    private String schemaRegistryUrlKey;
+    private String schemaRegistryUrl;
+    private Integer numOfPartitions;
+    private Short replicationFactor;
+}
---
+++ infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/kafka/config/data/KafkaProducerConfigData.java
@@ -0,0 +1,24 @@
+package com.food.ordering.system.kafka.config.data;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author kalvens on 4/5/23
+ * @project food-ordering-system
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "kafka-producer-config")
+public class KafkaProducerConfigData {
+    private String keySerializerClass;
+    private String valueSerializerClass;
+    private String compressionType;
+    private String acks;
+    private Integer batchSize;
+    private Integer batchSizeBoostFactor;
+    private Integer lingerMs;
+    private Integer requestTimeoutMs;
+    private Integer retryCount;
+}
